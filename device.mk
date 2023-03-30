756afa6b9cdfe76856a82227f4e1cc957555070b
blackshibe <kacperszczepan@tutanota.com>
2023-03-30 15:39:49 +0200

    disable encryption, adjust partitions

--- device.mk
+++ device.mk
@@ -2,9 +2,9 @@
 PRODUCT_SHIPPING_API_LEVEL := 29
 
 # qcom decryption
-PRODUCT_PACKAGES_ENG += \
-    qcom_decrypt \
-    qcom_decrypt_fbe
+# PRODUCT_PACKAGES_ENG += \
+#     qcom_decrypt \
+#     qcom_decrypt_fbe
 
 # Custom ROM asserts
 TARGET_OTA_ASSERT_DEVICE := pdx201
